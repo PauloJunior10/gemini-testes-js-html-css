3b16d38b12f9a9baeaf60db518337bc5e450eafe
PauloJunior10 <paulojunior.10@outlook.com>
2025-10-16 21:12:16 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const noBtn = document.getElementById('noBtn');
 const yesBtn = document.getElementById('yesBtn');
 const message = document.getElementById('message');
+const h1 = document.querySelector('h1');
+const buttonsDiv = document.querySelector('.buttons');
 
 let isAbsolute = false;
+let isConfirmed = false;
+let noClicked = false;
 
 noBtn.addEventListener('mouseover', () => {
     if (!isAbsolute) {
@@ -15,8 +19,29 @@ noBtn.addEventListener('mouseover', () => {
     noBtn.style.top = `${y}px`;
 });
 
-yesBtn.addEventListener('click', () => {
-    message.classList.remove('hidden');
+noBtn.addEventListener('click', () => {
+    h1.textContent = 'kkkkk você não tem escolha, casa comigo logo';
+    yesBtn.textContent = 'ACEITO';
     noBtn.classList.add('hidden');
-    yesBtn.classList.add('hidden');
+    buttonsDiv.style.justifyContent = 'center';
+    noClicked = true;
+});
+
+yesBtn.addEventListener('click', () => {
+    if (noClicked) {
+        message.classList.remove('hidden');
+        noBtn.classList.add('hidden');
+        yesBtn.classList.add('hidden');
+        h1.classList.add('hidden');
+    } else if (!isConfirmed) {
+        h1.textContent = 'Você tem certeza?';
+        yesBtn.textContent = 'Tenho certeza!';
+        noBtn.classList.add('hidden');
+        isConfirmed = true;
+    } else {
+        message.classList.remove('hidden');
+        noBtn.classList.add('hidden');
+        yesBtn.classList.add('hidden');
+        h1.classList.add('hidden');
+    }
 });
